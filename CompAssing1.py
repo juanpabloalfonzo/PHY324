45d3548f63981dd1279352747829f0a3b029e64e
Juan Pablo Alfonzo <juanpabloalfonzo@gmail.com>
2021-01-09 01:25:28 -0500

    changed to main

--- CompAssing1.py
+++ CompAssing1.py
@@ -2,9 +2,9 @@ import random as ra
 import numpy as np
 
 #Exercise 1
-X=np.zeros(1000)
-Y=np.zeros(1000)
-for i in range(1000): 
+X=np.zeros(10**4)
+Y=np.zeros(10**4)
+for i in range(10**4): 
     x=ra.uniform(-1,1) #Generating Position
     y=ra.uniform(-1,1)
     X[i]=x #Storing Position 
